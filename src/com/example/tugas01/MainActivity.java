d06ef687ea24f28706df899ae5a15bd26598e94f
AlvinLimz10 <57312681+AlvinLimz10@users.noreply.github.com>
2019-11-05 23:14:21 +0700

    Project Tugas01 add ListView

--- src/com/example/tugas01/MainActivity.java
+++ src/com/example/tugas01/MainActivity.java
@@ -1,52 +1,48 @@
 package com.example.tugas01;
 
-import android.os.Bundle;
+import android.R.string;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Intent;
-import android.view.Menu;
+import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
-	
-	Button bquiz1;
-	Button bquiz2;
 
-    @Override
+	ListView lvmenu;
+	String[] menu = {"Profil", "Quiz", "Keluar"};
+	
+	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
-        bquiz1 = (Button)findViewById(R.id.btnquiz1);
-        bquiz2 = (Button)findViewById(R.id.btnquiz2);
-        
-        bquiz1.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View arg0) {
-				// TODO Auto-generated method stub
-				Intent intent = new Intent(MainActivity.this,quiz1.class);
-				startActivity(intent);
-				
-			}});
-        bquiz2.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View arg0) {
-				// TODO Auto-generated method stub
-				Intent intent = new Intent(MainActivity.this,quiz2.class);
-				startActivity(intent);
-			}
-		});
-        
-    }
+	
+        lvmenu = (ListView)findViewById(R.id.lvm);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, menu);
+        lvmenu.setAdapter(adapter);
+        lvmenu.setOnItemClickListener(listClick);
+	}
+	
+	private AdapterView.OnItemClickListener listClick = new AdapterView.OnItemClickListener() {
 
+		@Override
+		public void onItemClick(AdapterView<?> arg0, View arg1, int arg2 ,
+				long arg3) {
+			// TODO Auto-generated method stub
+			if(arg2 == 0)
+			{Intent intent = new Intent(MainActivity.this,profile.class);
+			startActivity(intent);}
+			else if(arg2 == 1)
+			{Intent intent = new Intent(MainActivity.this,btnquiz.class);
+			startActivity(intent);}
+			else if(arg2 >= 2) 
+			{System.exit(0);}}
+		}
+		
+	;
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
 }
